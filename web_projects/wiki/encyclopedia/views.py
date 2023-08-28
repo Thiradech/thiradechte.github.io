12d1469ca800af4077e429504ec93c1145e8f25f
Thiradech <134780065+Thiradech@users.noreply.github.com>
2023-08-28 22:39:29 +0700

    modified:   wiki/encyclopedia/__pycache__/views.cpython-311.pyc 	modified:   wiki/encyclopedia/views.py

--- web_projects/wiki/encyclopedia/views.py
+++ web_projects/wiki/encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from markdown2 import Markdown
 from . import util
+import re
 
 def convert_md_to_html(title):
     content_md = util.get_entry(title)
@@ -32,12 +33,19 @@ def search(request):
     if request.method == "POST":
         title = request.POST['q']
         content_html = convert_md_to_html(title)
+        entries = util.list_entries()
+        new_entries = []
         if content_html is None:
-            return render(request, "encyclopedia/error.html", {
+            for entry in entries:
+                if re.search(title.lower(), entry.lower()) is not None:
+                    new_entries.append(entry)
+            return render(request, "encyclopedia/related_entries.html", {
+                "entries": new_entries,
                 "title": title
             })
-        else: 
+        else:
             return render(request, "encyclopedia/entry.html", {
-                "entry": content_html,
-                "title": title
-            })
+            "entry": content_html,
+            "title": title
+        })
+                    
