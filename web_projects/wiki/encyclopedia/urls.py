228d93d9870d5e582a103da478971def5be6431c
Thiradech <134780065+Thiradech@users.noreply.github.com>
2023-08-30 22:46:49 +0700

    final

--- web_projects/wiki/encyclopedia/urls.py
+++ web_projects/wiki/encyclopedia/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('new_page/', views.set_NewPage, name='new_page' ),
     path('create_newPage/', views.create_NewPage, name='create_newPage'),
     path('edit_page/<str:title>', views.edit_page, name='edit_page'),
-    path('edit_entry/<str:title>', views.edit_entry, name='edit_entry')
+    path('edit_entry/<str:title>', views.edit_entry, name='edit_entry'),
+    path('random_entry/', views.random_entry, name='random_entry')
 ]
--- web_projects/wiki/encyclopedia/views.py
+++ web_projects/wiki/encyclopedia/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from markdown2 import Markdown
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . import util
 import re
+import random
 
 def convert_md_to_html(title):
     content_md = util.get_entry(title)
@@ -63,13 +66,22 @@ def create_NewPage(request):
             return render(request, "encyclopedia/error.html", {
                 "error": error_message
             })
-        else:
+        elif title != '':
             util.save_entry(title=title, content=content)
+            content = convert_md_to_html(title)
             return render(request, "encyclopedia/entry.html", {
             "entry": content,
             "title": title
             })
+        elif title == '':
+            error_message = f"Please input valid title (title can not be empty!)"
+            return render(request, "encyclopedia/error.html", {
+                "error": error_message
+            })
+        else:
+            return render(request, "encyclopedia/error.html", )
             
+                            
 def edit_page(request, title):
     content = util.get_entry(title)
     return render(request, "encyclopedia/edit_entry.html", {
@@ -81,6 +93,22 @@ def edit_entry(request, title):
     if request.method == "POST":
         content = request.POST['content']    
         util.save_entry(title, content)
+        content = convert_md_to_html(title)
+        return render(request, "encyclopedia/entry.html", {
+            "entry": content,
+            "title": title 
+        })
+
+def random_entry(request):
+    length = len(util.list_entries())
+    rand_n = random.randint(0, length-1)
+    title = util.list_entries()[rand_n]
+    content_html = convert_md_to_html(title)
+    return render(request, "encyclopedia/entry.html", {
+            "entry": content_html,
+            "title": title
+            })
+    
 
             
             
