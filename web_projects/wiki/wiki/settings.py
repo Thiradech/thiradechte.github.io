68b896201151d34d9d2ef1d8e8cda494a59f7f26
Thiradech <thiradech.te@gmail.com>
2023-08-31 09:53:06 +0000

    add commerce project

--- web_projects/wiki/wiki/settings.py
+++ web_projects/wiki/wiki/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '%710m*zic)#0u((qugw#1@e^ty!c)9j04956v@ly(_86n$rg)h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '8000-thiradech-thiradechtegi-wj7e8qq2474.ws-us104.gitpod.io'
+]
 
 
 # Application definition
